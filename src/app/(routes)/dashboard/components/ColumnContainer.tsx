5d7e080dabbb0658fffc3dd9993f077a92b6bdbd
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-06-06 12:14:57 +0530

    Added Draggable property and drag overlay to the columns

--- src/app/(routes)/dashboard/components/ColumnContainer.tsx
+++ src/app/(routes)/dashboard/components/ColumnContainer.tsx
@@ -1,15 +1,25 @@
+"use client";
 import { Card, CardHeader } from "@/components/ui/card";
 import { DeleteIcon, Edit2Icon, TrashIcon } from "lucide-react";
 import React from "react";
 import DeleteColumnBtn from "./DeleteColumnBtn";
 import { Column } from "@/lib/store";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface ColumnContainerProps {
   column: Column;
 }
 export default function ColumnContainer({ column }: ColumnContainerProps) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: column.id, data: { type: "column", column: column } });
+
+  const style = {
+    transition: transition,
+    transform: CSS.Transform.toString(transform),
+  };
   return (
-    <Card>
+    <Card ref={setNodeRef} {...attributes} {...listeners} style={style}>
       <CardHeader>
         <div className="bg-slate-100 p-3 flex items-center justify-between rounded-md">
           <h3 className="text-xl font-bold text-black">{column.name}</h3>
--- src/app/(routes)/dashboard/components/KanbanBoard.tsx
+++ src/app/(routes)/dashboard/components/KanbanBoard.tsx
@@ -1,20 +1,31 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { ListPlusIcon, PlusCircleIcon, PlusIcon } from "lucide-react";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import ColumnContainer from "./ColumnContainer";
 import { useColumnStore } from "@/lib/store";
-
+import { DndContext, DragOverlay, DragStartEvent } from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
 type Column = {
   name: string;
   id: string;
 };
 
 export default function KanbanBoard() {
-  // const [columns, setColumns] = React.useState<Column[]>([]);
+  // Zustand States
   const columns = useColumnStore((state) => state.columns);
   const addColumn = useColumnStore((state) => state.addColumn);
+
+  //State Variables
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null);
+
+  const columnsId = useMemo(() => {
+    return columns.map((column) => column.id);
+  }, [columns]);
+
   const [name, setName] = React.useState("");
+
+  //Functions
 
   const createNewColumn = () => {
     const columnToAdd: Column = {
@@ -23,19 +34,36 @@ export default function KanbanBoard() {
     };
     addColumn(columnToAdd);
   };
+
+  const onDragStart = (e: DragStartEvent) => {
+    console.log(e);
+    if (e.active.data.current?.type === "column") {
+      setActiveColumn(e.active.data.current?.column);
+      return;
+    }
+  };
   return (
-    <div className="mt-5 flex items-center overflow-x-auto overflow-y-hidden">
-      <div className="container grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-        {columns.map((column) => (
-          <ColumnContainer key={column.id} column={column} />
-        ))}
-        <Button variant={"outline"} size={"lg"} onClick={createNewColumn}>
-          <div className="flex items-center gap-3">
-            <PlusCircleIcon className="h-6 w-6" />
-            Create Column
-          </div>
-        </Button>
+    <DndContext onDragStart={onDragStart}>
+      <div className="mt-5 flex items-center overflow-x-auto overflow-y-hidden">
+        <div className="container grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+          {/** It requires that you pass it a sorted array of the unique identifiers associated with the elements that use the SortableContext */}
+          <SortableContext items={columnsId}>
+            {columns.map((column) => (
+              <ColumnContainer key={column.id} column={column} />
+            ))}
+          </SortableContext>
+          <Button variant={"outline"} size={"lg"} onClick={createNewColumn}>
+            <div className="flex items-center gap-3">
+              <PlusCircleIcon className="h-6 w-6" />
+              Create Column
+            </div>
+          </Button>
+        </div>
       </div>
-    </div>
+      {/** Drag Overlay shows the shadow of the column that we are dragging*/}
+      <DragOverlay>
+        {activeColumn && <ColumnContainer column={activeColumn} />}
+      </DragOverlay>
+    </DndContext>
   );
 }
